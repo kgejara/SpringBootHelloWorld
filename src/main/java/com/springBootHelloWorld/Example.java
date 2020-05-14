6116fafcbd0d59a0d16c0a7ce11092b983c6a172
kgejara <47546861+kgejara@users.noreply.github.com>
2020-05-14 18:43:30 -0500

    Add files via upload

---
+++ src/main/java/com/springBootHelloWorld/Example.java
@@ -0,0 +1,32 @@
+package com.springBootHelloWorld;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping( "/{name}" )
+    String getName( @PathVariable( "name" ) String urlName ) {
+        return "Hello " + urlName.toUpperCase();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
+
+
+
+
+
+
